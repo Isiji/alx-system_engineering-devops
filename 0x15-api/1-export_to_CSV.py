25d6a46ed1e4088140f61839f3b5693cff3a5293
isiji <blairisiji@gmail.com>
2024-03-25 22:32:01 +0300

    export

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,29 +1,56 @@
 #!/usr/bin/python3
-#Records all tasks that are owned by this employee
-#Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-#File name must be: USER_ID.csv
+"""
+Records all tasks that are owned by this employee.
+
+Format:
+    "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+
+File name:
+    USER_ID.csv
+
+Usage:
+    python3 script_name.py USER_ID
+
+Arguments:
+    USER_ID: The ID of the employee whose tasks need to be recorded.
+
+Dependencies:
+    - requests
+    - csv
+
+Example:
+    python3 script_name.py 2
+    This will record all tasks owned by the employee with ID 2 into
+    a CSV file named "2.csv".
+"""
 
 import requests
 import sys
 import csv
 
 if __name__ == "__main__":
-    #can not be executed if imported
+    # The base URL of the API
     url = "https://jsonplaceholder.typicode.com/"
-    #exctracting user id from the command line arguments
+
+    # Extracting user ID from the command line arguments
     user_id = sys.argv[1]
-    #getting the user response details
+
+    # Getting the user details
     user_response = requests.get(url + "users/{}".format(user_id))
     user = user_response.json()
-    #getting the username
+
+    # Getting the username
     username = user.get("username")
-    #getting the to do list
+
+    # Getting the to-do list for the user
     params = {"userId": user_id}
     todos_response = requests.get(url + "todos", params=params)
     todos = todos_response.json()
-    #open a csv file
+
+    # Open a CSV file to write the tasks
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        
         for todo in todos:
-            writer.writerow([user_id, username,todo.get("completed"),
+            writer.writerow([user_id, username, todo.get("completed"),
                              todo.get("title")])
