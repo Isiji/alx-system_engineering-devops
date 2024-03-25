c17b43d5f0cab78e1453d0a7dfa93d8d829768a0
isiji <blairisiji@gmail.com>
2024-03-25 04:10:33 -0700

    get data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 #this script gets data of  an emloyee through an API
+#display on the standard output the employee TODO list
+#progress in this exact format:
+#First line: Employee EMPLOYEE_NAME is done with tasks
+#(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+#EMPLOYEE_NAME: name of the employee
+#NUMBER_OF_DONE_TASKS: number of completed tasks
+#TOTAL_NUMBER_OF_TASKS: total number of tasks,
+#which is the sum of completed and non-completed tasks
+#Second and N next lines display the title of completed tasks:
+#TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE
+
 import requests
 import sys
 
@@ -19,7 +30,8 @@ if __name__ == "__main__":
     for todo in todos:
         if todo.get("completed") is True:
             completed.append(todo.get("title"))
-    print("Employee {} is done with tasks{}/{}".format(user.get("name"),len(completed),len(todos)))
+    print("Employee {} is done with tasks{}/{}".format(user.get("name"),
+        len(completed),len(todos)))
 
     for complete in completed:
         print(f"\t {complete}")
