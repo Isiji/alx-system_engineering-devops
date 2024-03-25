afd859f547759f0109a411ed52e439480be84588
isiji <blairisiji@gmail.com>
2024-03-25 23:12:34 +0300

    del

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -73,7 +73,3 @@ if __name__ == "__main__":
     with open(filename, "w") as jsonfile:
         json.dump(data_to_export, jsonfile, indent=4)
 
-    print("Correct USER_ID: OK")
-    print("USER_ID's value type is a list of dicts: OK")
-    print("All tasks found: OK")
-
