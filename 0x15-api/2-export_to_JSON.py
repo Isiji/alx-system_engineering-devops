e18e557ffd34a6bd7c54735a849d7ba63d1dbb65
isiji <blairisiji@gmail.com>
2024-03-25 23:09:21 +0300

    json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -34,8 +34,8 @@ Dependencies:
 
 Example:
     python3 script_name.py 2
-    This will record all tasks owned by the employee with ID 2
-    into a JSON file named "2.json".
+    This will record all tasks owned by the employee with ID 2 into
+    a JSON file named "2.json".
 """
 
 import json
@@ -51,13 +51,7 @@ if __name__ == "__main__":
 
     # Retrieving user data
     user_response = requests.get(url + "users/{}".format(user_id))
-    user_data = user_response.json()
-
-    # Check if user ID retrieved from API matches provided user ID
-    if str(user_data['id']) != str(user_id):
-        print("Incorrect USER_ID: Expected {}, Got {}".format(user_id,
-                                                    user_data['id']))
-        sys.exit(1)
+    user = user_response.json()
 
     # Retrieving tasks for the user
     params = {"userId": user_id}
@@ -70,23 +64,13 @@ if __name__ == "__main__":
         task_info = {
             "task": todo.get("title"),
             "completed": todo.get("completed"),
-            "username": user_data.get("username")
+            "username": user.get("username")  # Use user's username
         }
         data_to_export[user_id].append(task_info)
 
-    # Check if USER_ID's value type is a list of dicts
-    if not isinstance(data_to_export[user_id],
-                      list) or not all(isinstance(task, dict)
-                                                    for task in data_to_export[user_id]):
-            print("USER_ID's value type is not a list of dicts")
-            sys.exit(1)
-
-    # Check if all tasks found in list of dicts
-    if len(todos) != len(data_to_export[user_id]):
-        print("All tasks not found")
-        sys.exit(1)
-
     # Writing data to a JSON file
-    with open("{}.json".format(user_id), "w") as jsonfile:
+    filename = "{}.json".format(user_id)
+    with open(filename, "w") as jsonfile:
         json.dump(data_to_export, jsonfile, indent=4)
-    
+
+    print(f"Data exported to {filename}")
