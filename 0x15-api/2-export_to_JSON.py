b6dddda18b7290a01d445a911813a627568343dc
isiji <blairisiji@gmail.com>
2024-03-25 22:55:07 +0300

    json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -50,12 +50,19 @@ if __name__ == "__main__":
     user_id = sys.argv[1]
 
     # Retrieving user data
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
+    user_response = requests.get(url + "users/{}".format(user_id))
+    user_data = user_response.json()
+
+    # Check if user ID retrieved from API matches provided user ID
+    if str(user_data['id']) != str(user_id):
+        print("Incorrect USER_ID: Expected {}, Got {}".format(user_id,
+                                                    user_data['id']))
+        sys.exit(1)
 
     # Retrieving tasks for the user
     params = {"userId": user_id}
-    todos = requests.get(url + "todos", params=params).json()
+    todos_response = requests.get(url + "todos", params=params)
+    todos = todos_response.json()
 
     # Creating data to export in JSON format
     data_to_export = {user_id: []}
@@ -63,10 +70,23 @@ if __name__ == "__main__":
         task_info = {
             "task": todo.get("title"),
             "completed": todo.get("completed"),
-            "username": username
+            "username": user_data.get("username")
         }
         data_to_export[user_id].append(task_info)
+
+    # Check if USER_ID's value type is a list of dicts
+    if not isinstance(data_to_export[user_id],
+                      list) or not all(isinstance(task, dict)
+                                                    for task in data_to_export[user_id]):
+            print("USER_ID's value type is not a list of dicts")
+            sys.exit(1)
+
+    # Check if all tasks found in list of dicts
+    if len(todos) != len(data_to_export[user_id]):
+        print("All tasks not found")
+        sys.exit(1)
 
     # Writing data to a JSON file
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump(data_to_export, jsonfile, indent=4)
+    
